ad51c4f2f25dda8f45e03a42b1f93169ab2eaff9
Rosario <rosario.a.robinson@gmail.com>
2022-07-08 13:50:39 -0400

    Express website, Module2 updates to hbs

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
 
 // Edited: inserted 'app_server' in path
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
 
-var app = express();
+const app = express(); 
 
 // view engine setup
 // insert 'app_server' in path
@@ -17,7 +18,7 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 // Inserted call to register the partials folder
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 app.set('view engine', 'hbs');
 
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 // Adding to pass request for the site default starting page over to the new main controller
-const ctrlMain = require('..controllers/main');
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
