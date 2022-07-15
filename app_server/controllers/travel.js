d2b9eaa5025f27849495ba0e997a2c8ca43ac1fa
Rosario <rosario.a.robinson@gmail.com>
2022-07-14 21:37:13 -0400

    Created JSON file to use dynamically for Express website

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,13 @@
 // Created file for Travlr page
+
+// Edited file to use NodeJS file system to retrieve JSON data from trips.json
+var fs = require('fs');
+
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', {title: 'Travlr Getaways', trips}); // Added trips data to pass to handlebars views
 };
 
 module.exports = {
