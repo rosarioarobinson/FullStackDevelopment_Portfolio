6c26b8f05c9743c62f75dc3ae523b28e0714a667
Rosario <rosario.a.robinson@gmail.com>
2022-07-23 23:43:02 -0400

    Created database files, installed Mongoose, and added code for database and schema

---
+++ .seedgooserc.js
@@ -0,0 +1,11 @@
+// .seedgooserc.js
+// Created and added seedgoose file for database
+module.exports = {
+    modelBaseDirectory: "app_server/database/models", // model directory name
+  
+    models: ["*.js", "!db.js"], // model matcher
+  
+    data: "data", // data directory name
+  
+    db: "mongodb://localhost:27017/travlr", // db connection url
+  };
