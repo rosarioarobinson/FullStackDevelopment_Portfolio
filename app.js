144ea7d15f8f5e635dce756b565edf6a258af6de
Rosario <rosario.a.robinson@gmail.com>
2022-08-04 00:57:42 -0400

    Pushing module 6 assignment

--- app.js
+++ app.js
@@ -24,12 +24,23 @@ app.set("views", path.join(__dirname, "app_server", "views"));
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
 app.set("view engine", "hbs");
-
+ 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Allow CORS
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
